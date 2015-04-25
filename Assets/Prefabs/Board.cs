edd54324de8d4e24257834eb0bf57502ffc892bf
YordanL <yordanlyudmilov@gmail.com>
2015-04-25 17:25:22 +0300

    + putting it in (the Board)

--- Assets/Prefabs/Board.cs
+++ Assets/Prefabs/Board.cs
@@ -5,6 +5,18 @@ public class Board : MonoBehaviour {
 	MovieTexture movie;
 	AudioSource auS;
 
+	public void loadVideo(string name) {
+		Renderer r = GetComponentInChildren<Renderer>();
+		movie = (MovieTexture)Resources.Load(name + "V");
+		r.material.mainTexture = movie;
+
+		AudioSource audS = GetComponentInChildren<AudioSource>();
+		AudioClip audClip = (AudioClip)Resources.Load(name + "A");
+		audS.clip = audClip;
+
+		//pauseVideo();
+	}
+
 	public void playVideo() {
 		movie.Play ();
 		auS.Play ();
@@ -14,12 +26,16 @@ public class Board : MonoBehaviour {
 		movie.Pause ();
 		auS.Pause ();
 	}
+
+	public void stopVideo() {
+		movie.Stop ();
+		auS.Stop ();
+	}
 	
 	void Start () {
 		Renderer r = gameObject.GetComponentInChildren<Renderer>();
 		movie = (MovieTexture)r.material.mainTexture;
 		movie.Pause ();
-
 		auS = gameObject.GetComponentInChildren<AudioSource>();
 		auS.Pause ();
 	}
