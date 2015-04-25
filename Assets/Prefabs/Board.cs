5c81556f96b4c75a9146f82e48ab0ea63fd28e5d
YordanL <yordanlyudmilov@gmail.com>
2015-04-25 03:53:08 +0300

    + Added basic Board functionality

---
+++ Assets/Prefabs/Board.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Board : MonoBehaviour {
+	MovieTexture movie;
+	AudioSource auS;
+
+	public void playVideo() {
+		movie.Play ();
+		auS.Play ();
+	}
+	
+	public void pauseVideo() {
+		movie.Pause ();
+		auS.Pause ();
+	}
+	
+	void Start () {
+		Renderer r = gameObject.GetComponentInChildren<Renderer>();
+		movie = (MovieTexture)r.material.mainTexture;
+		movie.Pause ();
+
+		auS = gameObject.GetComponentInChildren<AudioSource>();
+		auS.Pause ();
+	}
+	
+	void Update () {
+		if (Input.GetButtonDown ("Jump")) {
+			if(movie.isPlaying){
+				pauseVideo();
+			}
+			else {
+				playVideo();
+			}
+		}
+	}
+}
