08f058db47ae58fc1448c24bcd7d02751935694d
Andon Mitsov <donimicov@gmail.com>
2015-04-25 17:02:37 +0300

    Revert "Revert "+added crosshair""
    This reverts commit 9be396db0ed3ce455f4f231172810e1cd6ad0cdb.

---
+++ Assets/VRScripts/GUIBehaivourScript.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class GUIBehaivourScript : MonoBehaviour{
+	private RectTransform rectTransformComponent;
+	private EventTrigger evTriggerComponent;
+
+	void Awake(){
+		rectTransformComponent = GetComponentInParent<RectTransform> ();
+		evTriggerComponent = GetComponentInParent<EventTrigger> ();
+	}
+
+	void Start(){
+		EventTrigger.Entry evEntry = new EventTrigger.Entry();
+		evEntry.eventID = EventTriggerType.PointerEnter;
+		evEntry.callback.AddListener ((eventData) => {ScaleCanvas(eventData);});
+		evTriggerComponent.delegates.Add (evEntry);
+
+
+
+	}
+
+	public void ScaleCanvas(BaseEventData ed){
+		rectTransformComponent.transform.localScale = rectTransformComponent.transform.localScale * 1.5f;
+	}
+}
