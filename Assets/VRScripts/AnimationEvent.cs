c1494d8f73c4c52f54dc60bcc82d08701b0617d3
Andon Mitsov <donimicov@gmail.com>
2015-04-26 11:10:14 +0300

    +character animation added

---
+++ Assets/VRScripts/AnimationEvent.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class AnimationEvent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	//Animation grab event
+	public static void OnGrab(){
+		Debug.LogWarning ("Grabbing mode");
+	}
+}
---
+++ Assets/VRScripts/CharacterAnimation.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CharacterAnimation : MonoBehaviour {
+	private Animator charAnim;
+	private AnimationEvent eventGrab;
+	public Transform bone;
+	// Use this for initialization
+	void Start () {
+		charAnim = GetComponent<Animator> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyUp (KeyCode.E)) {
+			Debug.LogWarning ("E pressed");
+			charAnim.SetBool ("shouldGrab",true);
+		}
+	}
+
+	public void OnGrab(){
+		foreach (Transform tr in gameObject.GetComponentsInChildren<Transform>()) {
+			Debug.LogWarning (tr);
+		}
+		charAnim.SetBool ("shouldGrab",false);
+	}
+}
