1f8a1fafe6e2c12a48989231aafb2a382dc1ed9c
Andon Mitsov <donimicov@gmail.com>
2015-04-26 12:11:24 +0300

    +added TaskManager

--- Assets/TaskManager.cs
+++ Assets/TaskManager.cs
@@ -2,6 +2,15 @@
 using System.Collections;
 
 public class TaskManager : MonoBehaviour {
+	public enum TaskState
+	{
+		Idle,
+		Running,
+		Finished
+	}
+
+	private static TaskState taskStatus;
+	private static GameObject activeTask;
 
 	// Use this for initialization
 	void Start () {
@@ -10,6 +19,33 @@ public class TaskManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
-	
+		if (taskStatus == TaskState.Finished)
+			taskStatus = TaskState.Idle;
 	}
+
+	public static GameObject GetTask(){
+		return activeTask;
+	}
+
+	public static string GetTaskName(){
+		return activeTask.name;
+	}
+
+	public static void EndTask(){
+		activeTask = null;
+		taskStatus = TaskState.Finished;
+	}
+
+	public static void StartTask(string name){
+		activeTask = (GameObject)Resources.Load (name);
+		GameObject table = GameObject.Find ("Table");
+		activeTask.transform.parent = table.transform;
+		activeTask.transform.position = new Vector3 (activeTask.transform.parent.position.x,
+		                                            activeTask.transform.parent.position.y + Vector3.up,
+		                                            activeTask.transform.parent.position.z);
+		taskStatus = TaskState.Running;
+	}
+
+
 }
+
